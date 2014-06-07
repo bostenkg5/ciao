3825ce3afc9bfa568cdf9e458392bd3621d3bccb
ej0cl6 <ej0cl6@gmail.com>
2014-06-07 16:50:25 +0800

    test_ej0cl6

---
+++ test_ej0cl6/main.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import threading
+import argparse
+from Tkinter import *
+
+def tt():
+	print 'tt'
+
+
+def main():
+	print 'main start'
+	
+	tkObj = Tk()
+	
+	button = Button(tkObj)
+	button["text"] = 'kkkkk'
+	button.grid(columnspan=10, sticky="nwse")
+	button["command"] = tt
+	
+	tkObj.mainloop()
+	
+if __name__ == "__main__":
+	main()
