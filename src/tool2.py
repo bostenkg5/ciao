f3ececcebb639b050778f64f03e61308914d1550
Chander Jaing <b99902060@ntu.edu.tw>
2014-06-10 20:45:38 +0800

    update GUI

--- src/tool2.py
+++ src/tool2.py
@@ -2,45 +2,43 @@ import os
 import sys
 from Tkinter import *
 from PIL import Image, ImageTk
+import random
 
-def startGame():
-	print 'main start'
-	
+def startGame(number):
+
 	tkObj = Tk()
-
-	canvas = Canvas(tkObj, width = 800, height = 400)
+	canvas = Canvas(tkObj, width = 1200, height = 400)
 	canvas.pack()
-	background = ImageTk.PhotoImage(file="background.jpg")
-	canvas.create_image(400,100,image=background)
-	x0 = 10		# initial left-most edge of ball
-	y0 = 180		# initial top-most edge of ball
-	x1 = 60		# inital right-most edge of ball
-	y1 = 230	# you've probably got the idea by now.
+	background = ImageTk.PhotoImage(master = canvas,file="background.jpg")
+	canvas.create_image(600,100,image=background)
+	x0 = 10
+	y0 = []
 	dx = 2
 	noBall = True
-	# create ball:
+	image = []
+	im = []
+	for i in range(number):
+		image.append(Image.open("ball"+str(i)+".png"))
+		image[i] = image[i].resize((100,100),Image.BICUBIC)
+		im.append(ImageTk.PhotoImage(image[i],master = canvas))
+		y0.append ((400/number)*i+50 )
+	
 	while True:
 		if noBall == True :
-			#which = canvas.create_oval(x0,y0,x1,y1,fill="blue", tag='blueBall')
-			image = Image.open("ball.png")
-			image = image.resize((200,200),Image.BICUBIC)
-			im=ImageTk.PhotoImage(image)
-			which = canvas.create_image(x0,y0,image=im,tag='blueBall')
+			index = random.randint(0,number-1)
+			print index
+			x0 = 10
+			which = canvas.create_image(x0,y0[index],image=im[index],tag='ball1')
 			noBall = False
-		if x0 >= 600:
+		if x0 >= 1000:
 			canvas.delete(which)
 			noBall = True
-			x0 = 10		# initial left-most edge of ball
-			y0 = 180		# initial top-most edge of ball
-			x1 = 60		# inital right-most edge of ball
-			y1 = 230	# you've probably got the idea by now.
 		else :
 			x0 += dx
-			x1 += dx
-			canvas.move('blueBall', dx, 0)
+			canvas.move('ball1', dx, 0)
 			canvas.after(10)
 			canvas.update()
-		
+
 	tkObj.mainloop()
 if __name__ == "__main__":
-	startGame()
+	startGame(4)
